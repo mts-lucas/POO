fdf3c2cdacd381c81d23af6bde887b724ab218c4
Lucas Mateus <lmateus1067@outlook.com>
2023-04-03 18:16:41 -0300

    faltando botoes

--- dart/estudos/app_de_estudos/lib/main.dart
+++ dart/estudos/app_de_estudos/lib/main.dart
@@ -2,39 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   MaterialApp app = MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.deepPurple),
-    home: Scaffold(
-        appBar: AppBar(title: Text("Hello World")),
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World"),
+          centerTitle: true,
+          titleSpacing: 0.0,
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment:
                 MainAxisAlignment.center, // adicionando o mainAxisAlignment
-            children: [
-              Text("Texto1"),
-              Text("Texto2"),
-              Text("Texto3"),
-              Text("Texto4"),
+            children: const [
+              Text("Texto1", style: TextStyle(fontWeight: FontWeight.bold)),
+              Text("Texto2", style: TextStyle(color: Colors.red)),
+              Text("Texto3", style: TextStyle(fontWeight: FontWeight.bold)),
+              Text("Texto4", style: TextStyle(color: Colors.red)),
             ],
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "person",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_basket),
-              label: "shopping",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
-              label: "favorite",
-            ),
-          ],
-        )),
-  );
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const <Widget>[],
+        ),
+      ));
 
   runApp(app);
 }
