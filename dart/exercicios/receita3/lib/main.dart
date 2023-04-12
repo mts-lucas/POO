a90f9a54a35560da330ffd2251a729f901bc7c32
Lucas Mateus <lmateus1067@outlook.com>
2023-04-11 23:36:35 -0300

    implementação com drawser

--- dart/exercicios/receita3/lib/main.dart
+++ dart/exercicios/receita3/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         ),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
+          drawer: NavDrawer(),
           appBar: AppBar(
             title: const Text("Dicas"),
           ),
@@ -73,5 +74,63 @@ class DataBodyWidget extends StatelessWidget {
                   child: Center(child: Text(obj)),
                 ))
             .toList());
+  }
+}
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+            ),
+            // child: Text(
+            //   'Colors Menu',
+            //   style: TextStyle(color: Colors.white, fontSize: 25),
+            // ),
+            child: Container(
+              height: 100,
+              child: const Center(
+                child: Text(
+                  'Colors Menu',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.green,
+            child: ListTile(
+              leading: Icon(Icons.format_paint),
+              title: Text('Green'),
+              onTap: () => {},
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            child: ListTile(
+              leading: Icon(Icons.format_paint),
+              title: Text('Red'),
+              onTap: () => {},
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            child: ListTile(
+              leading: Icon(Icons.format_paint),
+              title: Text('Blue'),
+              onTap: () => {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
