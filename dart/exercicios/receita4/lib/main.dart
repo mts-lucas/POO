77617dcb2b3c4731944ef498da4615473f7500c3
Lucas Mateus <lmateus1067@outlook.com>
2023-04-22 16:36:52 -0300

    ponto 2

--- dart/exercicios/receita4/lib/main.dart
+++ dart/exercicios/receita4/lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+var dataObjects = [
+  {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
+  {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
+  {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
+  {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
+  {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
+  {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
+  {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
+  {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
+  {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
+  {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
+  {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
+  {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
+];
+
 void main() {
   MyApp app = MyApp();
 
@@ -16,11 +31,9 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text("Dicas"),
           ),
-          body: DataBodyWidget(objects: [
-            "La Fin Du Monde - Bock - 65 ibu",
-            "Sapporo Premiume - Sour Ale - 54 ibu",
-            "Duvel - Pilsner - 82 ibu"
-          ]),
+          body: MyTileWidget(
+            objects: dataObjects,
+          ),
           bottomNavigationBar: NewNavBar(),
         ));
   }
@@ -48,23 +61,55 @@ class NewNavBar extends StatelessWidget {
 }
 
 class DataBodyWidget extends StatelessWidget {
-  List<String> objects;
+  List objects;
 
   DataBodyWidget({this.objects = const []});
 
-  Expanded processarUmElemento(String obj) {
-    return Expanded(
-      child: Center(child: Text(obj)),
-    );
+  @override
+  Widget build(BuildContext context) {
+    var columnNames = ["Nome", "Estilo", "IBU"],
+        propertyNames = ["name", "style", "ibu"];
+
+    return ListView(children: [
+      DataTable(
+          columns: columnNames
+              .map((name) => DataColumn(
+                  label: Expanded(
+                      child: Text(name,
+                          style: TextStyle(fontStyle: FontStyle.italic)))))
+              .toList(),
+          rows: objects
+              .map((obj) => DataRow(
+                  cells: propertyNames
+                      .map((propName) => DataCell(Text(obj[propName])))
+                      .toList()))
+              .toList())
+    ]);
   }
+}
+
+class MyTileWidget extends StatelessWidget {
+  final List objects;
+
+  MyTileWidget({this.objects = const []});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: objects
-            .map((obj) => Expanded(
-                  child: Center(child: Text(obj)),
-                ))
-            .toList());
+    var propertyNames = ["name", "style", "ibu"];
+
+    return ListView(
+      children: objects
+          .map(
+            (obj) => Card(
+              child: ListTile(
+                title: Text(obj[propertyNames[0]]),
+                subtitle: Text(
+                  "${obj[propertyNames[1]]}\n${obj[propertyNames[2]]}",
+                ),
+              ),
+            ),
+          )
+          .toList(),
+    );
   }
 }
