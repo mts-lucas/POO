ba7fb1f891f57bed8ea5806f3fbafe23987ad0e8
Lucas Mateus <lmateus1067@outlook.com>
2023-05-16 21:50:20 -0300

    preparando ambiente

--- dart/exercicios/receita6/lib/main.dart
+++ dart/exercicios/receita6/lib/main.dart
@@ -3,26 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class DataService {
-  final ValueNotifier<List> tableStateNotifier;
-  final ValueNotifier<List<String>> columnNamesNotifier;
-  final ValueNotifier<List<String>> propertyNamesNotifier;
-
-  DataService()
-      : tableStateNotifier = ValueNotifier([
-          {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
-          {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
-          {"name": "Duvel", "style": "Pilsner", "ibu": "82"}
-        ]),
-        columnNamesNotifier = ValueNotifier(["Nome", "Estilo", "IBU"]),
-        propertyNamesNotifier = ValueNotifier(["name", "style", "ibu"]);
+  final ValueNotifier<List> tableStateNotifier = new ValueNotifier([]);
 
   void carregar(index) {
-    final List<Function> funtions = [
-      carregarCafes,
-      carregarCervejas,
-      carregarNacoes,
-    ];
-    funtions[index]();
+    if (index == 1) carregarCervejas();
   }
 
   void carregarCervejas() {
@@ -31,28 +15,6 @@ class DataService {
       {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
       {"name": "Duvel", "style": "Pilsner", "ibu": "82"}
     ];
-    columnNamesNotifier.value = ["Nome", "Estilo", "IBU"];
-    propertyNamesNotifier.value = ["name", "style", "ibu"];
-  }
-
-  void carregarCafes() {
-    tableStateNotifier.value = [
-      {"name": "Santa Clara", "quality": "razoavel", "note": "10"},
-      {"name": "Marata", "quality": "ruim", "note": "2"},
-      {"name": "Serido", "quality": "horrivel", "note": "0"}
-    ];
-    columnNamesNotifier.value = ["Nome", "Qualidade", "Nota Pessoal"];
-    propertyNamesNotifier.value = ["name", "quality", "note"];
-  }
-
-  void carregarNacoes() {
-    tableStateNotifier.value = [
-      {"name": "Brasil", "style": "Samba", "ping": "10"},
-      {"name": "EUA", "style": "Rock", "ping": "3"},
-      {"name": "Nova Zelandia", "style": "blues", "ping": "8"}
-    ];
-    columnNamesNotifier.value = ["Nome", "Estilo Musical", "Nota da Pinga"];
-    propertyNamesNotifier.value = ["name", "style", "ping"];
   }
 }
 
@@ -78,8 +40,9 @@ class MyApp extends StatelessWidget {
               valueListenable: dataService.tableStateNotifier,
               builder: (_, value, __) {
                 return DataTableWidget(
-                  jsonObjects: value,
-                );
+                    jsonObjects: value,
+                    propertyNames: ["name", "style", "ibu"],
+                    columnNames: ["Nome", "Estilo", "IBU"]);
               }),
           bottomNavigationBar:
               NewNavBar(itemSelectedCallback: dataService.carregar),
@@ -88,11 +51,10 @@ class MyApp extends StatelessWidget {
 }
 
 class NewNavBar extends HookWidget {
-  var itemSelectedCallback;
+  final _itemSelectedCallback;
 
-  NewNavBar({this.itemSelectedCallback}) {
-    itemSelectedCallback ??= (_) {};
-  }
+  NewNavBar({itemSelectedCallback})
+      : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
 
   @override
   Widget build(BuildContext context) {
@@ -102,7 +64,7 @@ class NewNavBar extends HookWidget {
         onTap: (index) {
           state.value = index;
 
-          itemSelectedCallback(index);
+          _itemSelectedCallback(index);
         },
         currentIndex: state.value,
         items: const [
@@ -121,25 +83,29 @@ class NewNavBar extends HookWidget {
 class DataTableWidget extends StatelessWidget {
   final List jsonObjects;
 
-  DataTableWidget({required this.jsonObjects});
+  final List<String> columnNames;
+
+  final List<String> propertyNames;
+
+  DataTableWidget(
+      {this.jsonObjects = const [],
+      this.columnNames = const ["Nome", "Estilo", "IBU"],
+      this.propertyNames = const ["name", "style", "ibu"]});
 
   @override
   Widget build(BuildContext context) {
-    final columnNames = dataService.columnNamesNotifier.value;
-    final propertyNames = dataService.propertyNamesNotifier.value;
     return DataTable(
-      columns: columnNames
-          .map((name) => DataColumn(
-              label: Expanded(
-                  child: Text(name,
-                      style: const TextStyle(fontStyle: FontStyle.italic)))))
-          .toList(),
-      rows: jsonObjects
-          .map((obj) => DataRow(
-              cells: propertyNames
-                  .map((propName) => DataCell(Text(obj[propName])))
-                  .toList()))
-          .toList(),
-    );
+        columns: columnNames
+            .map((name) => DataColumn(
+                label: Expanded(
+                    child: Text(name,
+                        style: TextStyle(fontStyle: FontStyle.italic)))))
+            .toList(),
+        rows: jsonObjects
+            .map((obj) => DataRow(
+                cells: propertyNames
+                    .map((propName) => DataCell(Text(obj[propName])))
+                    .toList()))
+            .toList());
   }
 }
